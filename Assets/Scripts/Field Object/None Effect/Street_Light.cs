b3874fdd6efda85eefff3b26edde1620f5bea8e1
Ayin <144647450+Ayin66666@users.noreply.github.com>
2025-07-11 00:00:42 +0900

    25.07.11 작업 내역
    1. 가로등 2종 추가
    
    2. 맵 포프 작업
    
    3. 맵 디테일 업그레이드
       - 도로 제작중

---
+++ Assets/Scripts/Field Object/None Effect/Street_Light.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+public class Street_Light : MonoBehaviour
+{
+    [Header("---Setting---")]
+    [SerializeField] private Light[] light;
+    [SerializeField] private float maxIntensity;
+    [SerializeField] private float minIntensity;
+
+    [Header("Flicker Settings")]
+    [SerializeField] private float flickerDurationMin;
+    [SerializeField] private float flickerDurationMax;
+
+    [SerializeField] private float waitBetweenFlickerMin;
+    [SerializeField] private float waitBetweenFlickerMax;
+    private Coroutine coroutine;
+
+
+    private void OnEnable()
+    {
+        if (coroutine != null)
+            StopCoroutine(coroutine);
+
+        coroutine = StartCoroutine(OnOff());
+    }
+
+    private IEnumerator OnOff()
+    {
+        while(gameObject.activeSelf)
+        {
+            int flickerCount = Random.Range(2, 6);
+            for (int i = 0; i < flickerCount; i++)
+            {
+                for (int i2 = 0; i2 < light.Length; i2++)
+                {
+                    light[i2].intensity = Random.Range(minIntensity, maxIntensity * 0.4f);
+                }
+                yield return new WaitForSeconds(Random.Range(flickerDurationMin, flickerDurationMax));
+
+                // ´Ù½Ã ¹à°Ô
+                for (int i2 = 0; i2 < light.Length; i2++)
+                {
+                    light[1].intensity = maxIntensity;
+                }
+                yield return new WaitForSeconds(Random.Range(flickerDurationMin, flickerDurationMax));
+            }
+
+            yield return new WaitForSeconds(Random.Range(waitBetweenFlickerMin, waitBetweenFlickerMax));
+        }
+    }
+}
