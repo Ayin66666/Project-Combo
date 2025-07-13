7a54bff566550bdc54fad15bebf2e9353f233811
Ayin <144647450+Ayin66666@users.noreply.github.com>
2025-07-14 00:08:54 +0900

    25.07.13 작업 내역
    1. 메인 맵 제작 (90%)
       - 가로등 배치 완료
       - 프랍 그룹화 완료
       - Street_Light의 기능을 일반, 깜빡임 2종으로 변경
       - 2층 오브젝트 배치 완료
    
    2. 필요한 작업
       - 3층 오브젝트 배치
       - 베쉬 베이커로 오브젝트 통합
       - 오클루전 컬링 & 네비메쉬 세팅
       - 포스트 프로세싱 구역별 세팅 (1/7 완료)

--- Assets/Scripts/Field Object/None Effect/Street_Light.cs
+++ Assets/Scripts/Field Object/None Effect/Street_Light.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Street_Light : MonoBehaviour
 {
     [Header("---Setting---")]
-    [SerializeField] private Light[] light;
+    [SerializeField] private LightType lightType;
     [SerializeField] private float maxIntensity;
     [SerializeField] private float minIntensity;
+    [SerializeField] private Light[] lightObject;
+    private enum LightType { Normal, Flicker }
 
     [Header("Flicker Settings")]
     [SerializeField] private float flickerDurationMin;
     [SerializeField] private float flickerDurationMax;
-
     [SerializeField] private float waitBetweenFlickerMin;
     [SerializeField] private float waitBetweenFlickerMax;
     private Coroutine coroutine;
@@ -20,10 +21,16 @@ public class Street_Light : MonoBehaviour
 
     private void OnEnable()
     {
-        if (coroutine != null)
-            StopCoroutine(coroutine);
+        switch (lightType)
+        {
+            case LightType.Normal:
+                break;
 
-        coroutine = StartCoroutine(OnOff());
+            case LightType.Flicker:
+                if (coroutine != null) StopCoroutine(coroutine);
+                coroutine = StartCoroutine(OnOff());
+                break;
+        }
     }
 
     private IEnumerator OnOff()
@@ -33,16 +40,16 @@ public class Street_Light : MonoBehaviour
             int flickerCount = Random.Range(2, 6);
             for (int i = 0; i < flickerCount; i++)
             {
-                for (int i2 = 0; i2 < light.Length; i2++)
+                for (int i2 = 0; i2 < lightObject.Length; i2++)
                 {
-                    light[i2].intensity = Random.Range(minIntensity, maxIntensity * 0.4f);
+                    lightObject[i2].intensity = Random.Range(minIntensity, maxIntensity * 0.4f);
                 }
                 yield return new WaitForSeconds(Random.Range(flickerDurationMin, flickerDurationMax));
 
                 // ´Ù½Ã ¹à°Ô
-                for (int i2 = 0; i2 < light.Length; i2++)
+                for (int i2 = 0; i2 < lightObject.Length; i2++)
                 {
-                    light[1].intensity = maxIntensity;
+                    lightObject[1].intensity = maxIntensity;
                 }
                 yield return new WaitForSeconds(Random.Range(flickerDurationMin, flickerDurationMax));
             }
